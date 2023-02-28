4b255b58f78cae629bd892f064774bf420185234
Lennox Charles <lennyaiko17@gmail.com>
2023-02-28 21:50:07 +0100

    clean up and change some variable names

--- pages/events/[types]/[id].js
+++ pages/events/[types]/[id].js
@@ -1,9 +1,9 @@
 // the name of the file can be your choice but it needs to have square brackets
 
-const Page = () => {
+const EventPage = () => {
   return (
     <h1>Our Single Event</h1>
   )
 } 
 
-export default Page
+export default EventPage
--- pages/events/[types]/index.js
+++ pages/events/[types]/index.js
@@ -1,4 +1,4 @@
-const Page = () => {
+const EventsPerCityPage = () => {
     return (
       <div>
         <h1>Our Single Event</h1>
@@ -13,4 +13,4 @@ const Page = () => {
     )
   } 
   
-  export default Page
+  export default EventsPerCityPage
